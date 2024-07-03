1b97055a3c571bbde9fbfdea13262b931fbc72d7
Kevin Knopp <kknopp@apple.com>
2024-07-02 22:04:06 -0500

    fix(removed footer waves)

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
 import styles from "./footer.module.css";
 import Image from "next/image";
-import Waves from "../waves";
+// import Waves from "../waves";
 
 export default function Footer() {
 	return (
 		<Fragment>
-			<Waves />
+			{/* <Waves /> */}
 			<footer className={styles.footer}>
 					<Image src="/ark-logo-wordmark.svg" alt='ark' width={148} height={150} className={styles.ark}
 					style={{
