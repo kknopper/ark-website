41411aef2cf721dd635870c978d466dfdce3781f
Kevin Knopp <kknopp@apple.com>
2024-07-03 08:50:24 -0500

    fix(adding hidden form field)

--- src/app/components/contact/index.tsx
+++ src/app/components/contact/index.tsx
@@ -11,17 +11,18 @@ export default function Contact() {
 			<Section customClass={`section-contact ${styles.contact}`} id="contact">
 				<h2 className={styles.headline}>Contact</h2>
 				<form name="contact" method="POST" data-netlify="true" className={styles.form}>
+					<input type="hidden" name="form-name" value="contact" />
 					<p>
-						<label className={styles.label}>Your Name: <input className={styles.input} type="text" name="name" /></label>
+						<label className={styles.label} htmlFor="name">Name: <input className={styles.input} type="text" name="name" /></label>
 					</p>
 					<p>
-						<label className={styles.label}>Your Email: <input className={styles.input} type="email" name="email" /></label>
+						<label className={styles.label} htmlFor="email">Email: <input className={styles.input} type="email" name="email" /></label>
 					</p>
 					<p>
-						<label className={styles.label}>Your Company: <input className={styles.input} type="text" name="company" /></label>
+						<label className={styles.label} htmlFor="company">Company: <input className={styles.input} type="text" name="company" /></label>
 					</p>
 					<p>
-						<label className={styles.label}>Message: <textarea className={styles.textarea} name="message"></textarea></label>
+						<label className={styles.label} htmlFor="message">Message: <textarea className={styles.textarea} name="message"></textarea></label>
 					</p>
 					<p>
 						<Button type="submit" theme="blue" className={styles.button}>Submit</Button>
